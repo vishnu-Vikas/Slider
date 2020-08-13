cfc2be9d769f820fd283b6c7e138d4a496933618
vishnu-Vikas <48799205+vishnu-Vikas@users.noreply.github.com>
2020-08-13 12:46:42 +0530

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+angular.module('range', [])
+    .controller('myctrl', ['$scope', function ($scope) {
+        $scope.age = 18;
+        $scope.school;
+        $scope.max = 2020;
+        $scope.name;
+        $scope.state;
+        $scope.year = 1999;
+
+    }]);
